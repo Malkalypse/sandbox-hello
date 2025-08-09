bc8a44fcc9c98fb61893db54972e5ec0c7a5529c
Malkalypse <50023271+Malkalypse@users.noreply.github.com>
2025-08-09 13:30:16 -0500

    Snapshot: 2025-08-09 13:30:16

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Hello World</title>
+	<title>Hello World</title>
 </head>
 <body>
-  <h1><?php echo "Hello, Kevin!"; ?></h1>
+	<h1><?php echo "Hello, Kevin!"; ?></h1>
 </body>
 </html>
